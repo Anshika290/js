b72021c548742623b3b6ea4a27e0ceb3a5535354
Anshika290 <anshikasaxena200@gmail.com>
2022-04-06 13:13:24 +0530

    My sixth commit

---
+++ t2date.js
@@ -0,0 +1,14 @@
+ let today = new Date();
+ let dd = today.getDate();
+ let mm = today.getMonth();
+ let yyyy = today.getFullYear();
+ today =  mm + '-' + dd + '-' + yyyy;
+ console.log(today);
+ today =  mm + '/' + dd + '/' + yyyy;
+ console.log(today);
+ today =  dd + '-' + mm + '-' + yyyy;
+ console.log(today);
+ today =  dd + '/' + mm + '/' + yyyy;
+ console.log(today);
+
+
